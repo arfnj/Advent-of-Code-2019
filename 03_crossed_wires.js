54e0e27b586013b171d15d71ce15d7a4adda3777
Andrew Fechner <arfechner@gmail.com>
2019-12-08 22:57:32 -0500

    Solves Day 3 Part 2

--- 03_crossed_wires.js
+++ 03_crossed_wires.js
@@ -107,10 +107,15 @@ const bruteForce = () => {
   let horizontal = originColumn;
   let vertical = originRow;
   let closest = Infinity;
+  let closestSteps = Infinity;
+  let wire1TotalSteps = 0;
+  let wire2TotalSteps = 0;
+  let wire1StepRecord = {};
 
   inputs.wire1.split(',').forEach(entry => {
     const instruction = evaluate(entry);
     for (let i=0; i<instruction.units; i++) {
+      wire1TotalSteps ++;
       if (instruction.direction === 'U') {
         vertical --;
       } else if (instruction.direction === 'D') {
@@ -121,6 +126,7 @@ const bruteForce = () => {
         horizontal ++;
       }
       grid[vertical][horizontal] = 1;
+      wire1StepRecord[`${vertical}-${horizontal}`] = wire1StepRecord[`${vertical}-${horizontal}`] || wire1TotalSteps;
     }
   });
   horizontal = originColumn;
@@ -128,6 +134,7 @@ const bruteForce = () => {
   inputs.wire2.split(',').forEach(entry => {
     const instruction = evaluate(entry);
     for (let i=0; i<instruction.units; i++) {
+      wire2TotalSteps ++;
       if (instruction.direction === 'U') {
         vertical --;
       } else if (instruction.direction === 'D') {
@@ -141,13 +148,51 @@ const bruteForce = () => {
       if (grid[vertical][horizontal] === 1) {
         let manhattan = (Math.abs(originColumn-horizontal) + Math.abs(originRow-vertical));
         closest = Math.min(closest,manhattan);
+        closestSteps = Math.min(closestSteps,wire2TotalSteps + wire1StepRecord[`${vertical}-${horizontal}`]);
         grid[vertical][horizontal] === 3;
       } else {
         grid[vertical][horizontal] === 2;
       }
     }
   });
-  console.log(closest);
+  console.log('Part 1 answer:', closest);
+  console.log('Part 2 answer:', closestSteps);
 }
 
 bruteForce();
+
+/*
+--- Part Two ---
+It turns out that this circuit is very timing-sensitive; you actually need to minimize the signal delay.
+
+To do this, calculate the number of steps each wire takes to reach each intersection; choose the intersection where the
+sum of both wires' steps is lowest. If a wire visits a position on the grid multiple times, use the steps value from the
+first time it visits that position when calculating the total value of a specific intersection.
+
+The number of steps a wire takes is the total number of grid squares the wire has entered to get to that location,
+including the intersection being considered. Again consider the example from above:
+
+...........
+.+-----+...
+.|.....|...
+.|..+--X-+.
+.|..|..|.|.
+.|.-X--+.|.
+.|..|....|.
+.|.......|.
+.o-------+.
+...........
+In the above example, the intersection closest to the central port is reached after 8+5+5+2 = 20 steps by the first wire
+and 7+6+4+3 = 20 steps by the second wire for a total of 20+20 = 40 steps.
+
+However, the top-right intersection is better: the first wire takes only 8+5+2 = 15 and the second wire takes
+only 7+6+2 = 15, a total of 15+15 = 30 steps.
+
+Here are the best steps for the extra examples from above:
+
+R75,D30,R83,U83,L12,D49,R71,U7,L72
+U62,R66,U55,R34,D71,R55,D58,R83 = 610 steps
+R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = 410 steps
+What is the fewest combined steps the wires must take to reach an intersection?
+*/
